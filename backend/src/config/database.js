4c496a5a70bfcff202f12a1a837bc8813d758c20
Husnain <husnian.cs@gmail.com>
2025-12-12 21:30:36 +0500

    Formatting code with Prettier

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,31 +4,30 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = async () => {
+  try {
+    console.log("Connecting to the Database .......");
+    await mongoose.connect(process.env.DB_URL);
+
+    console.log("Database is connected a successfully");
+
+    // HANDLING INTERPT WHILE SCAN IS RUNNING AND SERVER RESTARTED 
     try {
-      console.log("Connecting to the Database .......");
-      await mongoose.connect(process.env.DB_URL);
-
- console.log("Database is connected a successfully")
-
-      try {
-        const Scan = mongoose.model('Scan');
-        const result = await Scan.updateMany({
-          status : "running"
+      const Scan = mongoose.model("Scan");
+      const result = await Scan.updateMany(
+        {
+          status: "running",
         },
-      { status : "failed",
-        results : "server is restarted"
+        { status: "failed", results: "server is restarted" }
+      );
+      if (result.modifiedCount > 0) {
+        console.log(`Fixed ${result.modifiedCount} stuck scans`);
       }
-    )
-             if (result.modifiedCount > 0) {
-                console.log(`Fixed ${result.modifiedCount} stuck scans`);
-            }
-        } catch (cleanupError) {
-            console.log("Could not cleanup scans:", cleanupError.message);
-        }
-      
+    } catch (cleanupError) {
+      console.log("Could not cleanup scans:", cleanupError.message);
+    }
 
-    } catch (error) {
-        console.error("The error of connecting database is",error);
-     };
+  } catch (error) {
+    console.error("The error of connecting database is", error);
+  }
 };
 export default connectDB;
